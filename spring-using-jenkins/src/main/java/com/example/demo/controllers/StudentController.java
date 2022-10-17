36a461b01227d4c2dfb93b6b55a430baa772428d
paiiisne <paiiisne@W5CG91117NL>
2022-10-17 11:11:59 +0530

    handson on jenkins

---
+++ spring-using-jenkins/src/main/java/com/example/demo/controllers/StudentController.java
@@ -0,0 +1,27 @@
+package com.example.demo.controllers;
+
+
+
+import java.util.Arrays;
+import java.util.List;
+
+
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+
+
+@RestController
+public class StudentController {
+
+
+
+   @GetMapping
+    public List<String> getNames(){
+        
+        return Arrays.asList("Ram","Shyam");
+        
+    }
+    
+}
